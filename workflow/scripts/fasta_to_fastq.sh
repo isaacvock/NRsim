34edd3d8ebf226b9b78d33e9ab71f63a87847753
isaacvock <isaac.vock@gmail.com>
2023-12-19 14:30:30 -0500

    Added print statements for debugging

--- workflow/scripts/fasta_to_fastq.sh
+++ workflow/scripts/fasta_to_fastq.sh
@@ -20,33 +20,38 @@ if [ "$PE" = "True" ]; then
 
     # parallel -j $cpus "python $pyscript -f {1} \
     #                                             --qscore $qscore" ::: ./results/split_fasta/sample_"$sample"_"$read".*.fasta
-    parallel -j $cpus "seqtk seq -F '\"$qscore\"' \
-                        {1} > ./results/split_fasta/sample_\"$sample\"_\"$read\".{#}.fastq" ::: ./results/split_fasta/sample_"$sample"_"$read".*.fasta
+    parallel -j $cpus "seqtk seq -F '#' {1} > ./results/split_fasta/sample_\"$sample\"_\"$read\".{#}.fastq" ::: ./results/split_fasta/sample_"$sample"_"$read".*.fasta
 
+    echo "converted fasta to fastq"
 
 
     parallel -j $cpus "python $pyscript2 -f {1} \
                                                 -k $kinetics" ::: ./results/split_fasta/sample_"$sample"_"$read".*.fastq
 
+    echo "Introduced NR-induced mutations in fastq files"
 
     cat ./results/split_fasta/sample_"$sample"_"$read".*.nrseq.fastq | pigz -p $cpus > "$output" 
 
+    echo "Concatenated fastq file"
 
 else
 
     # parallel -j $cpus "python $pyscript -f {1} \
     #                                             --qscore $qscore" ::: ./results/split_fasta/sample_"$sample".*.fasta
-    parallel -j $cpus "seqtk seq -F '\"$qscore\"' \
-                        {1} > ./results/split_fasta/sample_\"$sample\".{#}.fastq" ::: ./results/split_fasta/sample_"$sample".*.fasta
+    parallel -j $cpus "seqtk seq -F '#' {1} > ./results/split_fasta/sample_\"$sample\".{#}.fastq" ::: ./results/split_fasta/sample_"$sample".*.fasta
 
-
+    echo "converted fasta to fastq"
 
 
     parallel -j $cpus "python $pyscript2 -f {1} \
                                                 -k $kinetics" ::: ./results/split_fasta/sample_"$sample".*.fastq
 
+    echo "Introduced NR-induced mutations in fastq files"
+
 
     cat ./results/split_fasta/sample_"$sample".*.nrseq.fastq | pigz -p $cpus > "$output" 
+
+    echo "Concatenated fastq file"
 
 
 fi
